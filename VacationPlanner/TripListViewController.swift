21985f14fd8e32d02f2dbf0b8aa65dca3c4b5de6
Philip A Senger <philip.a.senger@cngrgroup.com>
2022-06-14 16:08:12 +1000

    * added Trip List View Controller and linked it to the view
    Signed-off-by: Philip A Senger <philip.a.senger@cngrgroup.com>

---
+++ VacationPlanner/TripListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TripListViewController.swift
+//  VacationPlanner
+//
+//  Created by Philip A Senger on 14/6/22.
+//
+
+import Cocoa
+
+class TripListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
