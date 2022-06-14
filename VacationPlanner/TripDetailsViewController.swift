8e91df946c3d5930bab2721098013c989c2fcfb3
Philip A Senger <philip.a.senger@cngrgroup.com>
2022-06-15 08:38:40 +1000

    Added more controllers
    Signed-off-by: Philip A Senger <philip.a.senger@cngrgroup.com>

---
+++ VacationPlanner/TripDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TripDetailsViewController.swift
+//  VacationPlanner
+//
+//  Created by Philip A Senger on 15/6/22.
+//
+
+import Cocoa
+
+class TripDetailsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ VacationPlanner/TripNotesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TripNotesViewController.swift
+//  VacationPlanner
+//
+//  Created by Philip A Senger on 15/6/22.
+//
+
+import Cocoa
+
+class TripNotesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
