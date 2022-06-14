246a35c8eeb1ec6b31e163c63210a178faec561e
Philip A Senger <philip.a.senger@cngrgroup.com>
2022-06-14 12:44:00 +1000

    Initial Commit

---
+++ VacationPlanner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VacationPlanner
+//
+//  Created by Philip A Senger on 14/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
