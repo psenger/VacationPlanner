f244aa3117641be74dde376908a727530fddc58b
Philip A Senger <philip.a.senger@cngrgroup.com>
2022-06-15 15:58:26 +1000

    Linked all the controls
    Signed-off-by: Philip A Senger <philip.a.senger@cngrgroup.com>

--- VacationPlanner/TripDetailsViewController.swift
+++ VacationPlanner/TripDetailsViewController.swift
@@ -9,9 +9,34 @@ import Cocoa
 
 class TripDetailsViewController: NSViewController {
 
+    @IBOutlet weak var startDatePicker: NSDatePicker!
+    @IBOutlet weak var endDatePicker: NSDatePicker!
+    @IBOutlet weak var totlaTimeLabel: NSTextField!
+    
+    lazy var dateComponentsFormatter: DateComponentsFormatter = {
+        let dateComponentsFormatter = DateComponentsFormatter()
+        dateComponentsFormatter.allowedUnits = .day
+        dateComponentsFormatter.unitsStyle = .full
+        return dateComponentsFormatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        self.startDatePicker.dateValue = Date()
+        self.endDatePicker.dateValue = Date()
+        dateChanged(self.startDatePicker)
     }
     
+    @IBAction func dateChanged(_ sender: NSDatePicker) {
+        let cal = Calendar( identifier: .gregorian)
+        let midnightStartdDate = cal.startOfDay(for: self.startDatePicker.dateValue)
+        
+        // make even days, we go thru midnight of the next day
+        let midnightEndDate = cal.startOfDay(for: self.endDatePicker.dateValue).addingTimeInterval(60 * 60 * 24 )
+        
+        let tripTime = midnightEndDate.timeIntervalSince(midnightStartdDate)
+        
+        self.totlaTimeLabel.stringValue = self.dateComponentsFormatter.string(from: tripTime)!
+    }
 }
